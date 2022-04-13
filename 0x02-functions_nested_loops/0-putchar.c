c8967aeb4c57c23b1d0d89ce9eaff78a7e9c2fdc
AKAB01 <akesireabdurrahmon>
2022-04-13 04:01:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: prints 'Holberton \n'
- * Return: Always 0 (success)
+ * Return: nothing, return void
  */
 
 int main(void)
 {
-char str[] = "Holberton\n";
-int i = 0;
-while (str[i] != '\0')
-{
-char (i);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }

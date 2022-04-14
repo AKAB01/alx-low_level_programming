886c422eb571d30f732d714d16fc613ec25e7680
AKAB01 <akesireabdurrahmon>
2022-04-14 15:04:11 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,11 +9,13 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-while (i < 10)
-{
 /*
- * while (i < 0)
+ * whilee (1 < 10)
+ * {
+ * Infinite loop _ No increase of variablee
+ * putchar (i);
 }
+*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }

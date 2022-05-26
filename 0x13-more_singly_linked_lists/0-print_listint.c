b90b594530ad8b5afcabd66aa40468a767709690
Abdurrahmon Akesire <101411673+AKAB01@users.noreply.github.com>
2022-05-26 13:10:57 +0100

    Print_Listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,22 @@
-#
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int nodes = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodes++;
+	}
+	return (nodes);
+}
